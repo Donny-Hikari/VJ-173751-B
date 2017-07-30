583ba604930aa841bc1617fdaefd2f4424bedfa5
Donny-Hikari <donny.hikari@gmail.com>
2017-07-30 12:21:09 +0900

    AC using RMQ with Sparse Table

--- main.cpp
+++ main.cpp
@@ -7,14 +7,24 @@
 #include <queue>
 #include <set>
 #include <map>
+#include <exception>
 using namespace std;
 
 const int MAXSD = 1e5 + 5;
-int D[6][MAXSD];
+int ST[21][6][MAXSD];
 int S[6];
 int n, m, k;
 
-bool chk(int md)
+void buildST()
+{
+    for (int o = 0; o < m; ++o)
+        for (int j = 1; j <= 20; ++j)
+            for (int i = 1; i <= n; ++i)
+                if (i + (1 << j) - 1 <= n)
+                    ST[j][o][i] = max(ST[j - 1][o][i], ST[j - 1][o][i + (1 << (j-1))]);
+}
+
+inline bool chk(int md)
 {
     bool bF = false;
     int lS[6];
@@ -23,7 +33,11 @@ bool chk(int md)
         int cnt = 0;
         for (int j = 0; j < m && cnt <= k; ++j)
         {
-            lS[j] = *max_element(D[j] + i, D[j] + i + md);
+            int iST = log2(md);
+            if ((1 << iST) < md)
+                lS[j] = max(ST[iST][j][i], ST[iST][j][i + md - (1 << iST)]);
+            else
+                lS[j] = ST[iST][j][i];
             cnt += lS[j];
         }
         if (cnt <= k)
@@ -37,13 +51,16 @@ bool chk(int md)
 
 int main()
 {
+    freopen("IN.txt", "r", stdin);
+
     while (scanf("%d %d %d", &n, &m, &k) != EOF)
     {
         for (int i = 1; i <= n; ++i)
             for (int j = 0; j < m; ++j)
-                scanf("%d", &D[j][i]);
+                scanf("%d", &ST[0][j][i]);
+        buildST();
         memset(S, 0, sizeof(S));
-        int l = 0, r = n;
+        int l = 1, r = n;
         while (r >= l)
         {
             int md = (r + l) >> 1;
